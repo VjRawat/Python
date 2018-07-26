178dca035257ced1cf3ecac784c2d35d58176253
Vijay Rawat <Vijay61rawat@gmail.com>
2018-07-26 16:57:34 +0530

    Program files added

---
+++ Intro to python.py
@@ -0,0 +1,9 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> firstNumber = 20.14
+>>> secondNumber = 33.13
+>>> 
+>>> result = firstNumber + secondNumber
+>>> print ("sum of two number is:" + str(result))
+sum of two number is:53.27
+>>> 
---
+++ Star Print.py
@@ -0,0 +1,35 @@
+#1
+for i in range(0,4):                        
+    for j in range(0,i):
+        print("*", end=" ")
+    print(" ")
+
+
+#2
+    
+for i in range(1,4):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print(" ")
+
+
+#3
+
+for i in range(1,4):
+    for j in range(i,4):
+        print("*", end=" ")
+    print(" ")
+
+
+
+#4
+
+for i in range(0,5):
+    for j in range(4,i,-1):
+        print(" ", end=" ")
+    for k in range(0,i):
+        print("*", end=" ")
+    if(i>1):
+        for l in range(1,i):
+            print("*", end=" ")
+    print(" ")
